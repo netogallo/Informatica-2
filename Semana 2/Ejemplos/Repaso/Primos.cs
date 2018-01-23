77fbd0222459f461ecb7bfa1263c7ab703c725c8
Javier Alvarez <calvarez098@gmail.com>
2018-01-23 08:01:07 -0600

    Agregando la funcion de obtener los numeros primos

--- Semana 2/Ejemplos/Repaso/Primos.cs
+++ Semana 2/Ejemplos/Repaso/Primos.cs
@@ -1,4 +1,32 @@
 public static class Primos{
     // Javier, jjalvarez
-    int[] Ejemplo1(int n);
+    public int[] Primos(int num)
+    {
+        int i, n = 2, cont = 0;
+        int[] numeros = new int[num];
+        while (cont < num)
+        {
+            i = 2;
+            while (i <= n)
+            {
+                if (i == n)
+                {
+                    numeros[cont] = n;
+                    cont = cont + 1;
+                    break;
+                }
+                else
+                {
+                    if (n % i == 0)
+                    {
+                        i = n;
+                        break;
+                    }
+                }
+                i = i + 1;
+            }
+            n = n + 1;
+        }
+        return numeros;
+    }
 }
