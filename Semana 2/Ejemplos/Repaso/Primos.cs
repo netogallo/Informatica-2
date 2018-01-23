61adb88c7512fb669b6696fab12225aff1c6bad8
Ernesto Rodriguez <neto@netowork.me>
2018-01-23 16:16:02 -0600

    Espacio para recibir los pull requests

--- Semana 2/Ejemplos/Repaso/Primos.cs
+++ Semana 2/Ejemplos/Repaso/Primos.cs
@@ -1,4 +1,10 @@
 public static class Primos{
     // Javier, jjalvarez
     int[] Ejemplo1(int n);
+
+    // Elisa elisammg
+    bool Ejemplo2(int n);
+
+    // Adrian gaitan171299
+    bool Ejemplo3(int n);
 }
