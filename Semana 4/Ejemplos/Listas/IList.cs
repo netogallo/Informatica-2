e0c31e6260cf94cd7a302844030cb71c320fe14d
Ernesto Rodriguez <neto@netowork.me>
2018-02-07 08:38:44 -0600

    Ejemplo listas

---
+++ Semana 4/Ejemplos/Listas/IList.cs
@@ -0,0 +1,6 @@
+public interface IList<T>
+{
+    T Get(int indice);
+    bool Set(int indice, T elemento);
+    void Push(T elemento);
+}
---
+++ Semana 4/Ejemplos/Listas/LinkedList.cs
@@ -0,0 +1,50 @@
+public class LinkedList<T>: IList<T>
+{
+    public T Elemento{get; private set;}
+    public LinkedList<T> Resto{get; private set;}
+
+    public LinkedList(T elemento)
+    {
+        this.Elemento = elemento;
+        this.Resto = null;
+    }
+
+    /* Todo: implementar */
+    public bool Set(int indice, T valor){ return false; }
+
+    public void Push(T elemento){
+        
+        LinkedList<T> lista = this;
+
+        while(lista.Resto != null)
+        {
+            lista = lista.Resto;
+        }
+
+        lista.Resto = new LinkedList<T>(elemento);
+    }
+
+    public T Get(int indice)
+    {
+        if(indice < 0)
+        {
+            return default(T);
+        }
+
+        int actualIndice = 0;
+        LinkedList<T> actual = this;
+
+        while(actualIndice++ < indice)
+        {
+
+            if(actual == null)
+            {
+                return default(T);
+            }
+
+            actual = actual.Resto;
+        }
+
+        return actual.Elemento;
+    }
+}
