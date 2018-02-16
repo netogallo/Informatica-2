8e5de3791fe1fa535163cc096c90270aacb77b00
Ernesto Rodriguez <neto@netowork.me>
2018-02-15 18:16:06 -0600

    Tarea #4

--- Semana 1/Ejemplos/Vehiculo/Program.cs
+++ Semana 1/Ejemplos/Vehiculo/Program.cs
@@ -4,6 +4,11 @@ namespace Vehiculo
 {
     class Program
     {
+
+        static void Test<T>(T arg){
+            Console.WriteLine(arg);
+        }
+
         static void Main(string[] args)
         {
             Vehiculo vehiculo = new Vehiculo("Carro");
---
+++ Semana 5/Hoja #4/Tupla.cs
@@ -0,0 +1,11 @@
+public class Tupla<T1,T2>{
+
+    public T1 Primero {get;}
+
+    public T2 Segundo {get;}
+
+    public Tupla(T1 primero, T2 segundo){
+        this.Primero = primero;
+        this.Segundo = segundo;
+    }
+}
