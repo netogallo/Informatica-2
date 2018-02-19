80a5d32488f3611d088944c0e2ecfac979382efd
Ernesto Rodriguez <neto@netowork.me>
2018-02-18 21:08:20 -0600

    Lambdas

---
+++ Semana 6/Ejemplos/Lambdas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Lambdas
+{
+    class Program
+    {
+
+        static void Ejemplo_Func(){
+        }
+
+        static void Ejemplo_Action(){
+            Func<int,int> suma = (x) => {return x+x;}
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
